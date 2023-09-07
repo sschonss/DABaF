e4722f9cb0b2dfe9aff562ee9c534e873de66bc8
sschonss <schonsluuiz@gmail.com>
2023-09-06 21:44:18 -0300

    Create a Api Route Room

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get(
+    '/rooms',
+    [App\Http\Controllers\RoomController::class, 'index']
+)->name('rooms.index');
