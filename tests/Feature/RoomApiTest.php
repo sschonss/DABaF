e81e355c0e5a582003daa535f2d62664b125639f
sschonss <schonsluuiz@gmail.com>
2023-09-06 21:45:15 -0300

    Create a Api Test Room

---
+++ tests/Feature/RoomApiTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RoomApiTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/rooms');
+
+    }
+}
